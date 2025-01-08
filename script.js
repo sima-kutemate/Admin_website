64ef011a4dfa2779197a43f328c1e088c9412899
sima-kutemate <simakutemate@gmail.com>
2025-01-08 23:49:47 +0530

    std.html

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function navigateTo(page) {
     window.location.href = page;
 }
+function toggleForm() { 
+    var formContainer = document.getElementById('form-container'); 
+    if (formContainer.style.display === 'none' || formContainer.style.display === '') { 
+        formContainer.style.display = 'block'; 
+    } else { formContainer.style.display = 'none'; 
+
+    } 
+}
